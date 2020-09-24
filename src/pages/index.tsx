408939ea7cc36f5f2226c7e3258442dbb6a7fcfe
Sheldon Kwok <me@sheldonk.com>
2020-09-24 03:21:05 -0700

    Add dropzone for uploading fantasy pros csv

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,43 @@
-import Head from "next/head";
-import styles from "../styles/Home.module.css";
+import React from "react";
+import Dropzone from "react-dropzone";
 
-export default function Home() {
-  return <div>Hello</div>;
+interface IProps {}
+
+interface IState {
+  contestID?: number;
+  fileData?: string;
+}
+
+export default class App extends React.Component<IProps, IState> {
+  constructor(props: IProps) {
+    super(props);
+
+    this.state = { contestID: undefined, fileData: "" };
+  }
+
+  async onDrop(acceptedFiles: File[]): Promise<void> {
+    const file = acceptedFiles[0];
+    const text = await file.text();
+
+    this.setState({ ...this.state, fileData: text });
+  }
+
+  render() {
+    if (this.state.fileData) {
+      return <div style={{ whiteSpace: "pre" }}>{this.state.fileData}</div>;
+    }
+
+    return (
+      <Dropzone onDrop={(acceptedFiles) => this.onDrop(acceptedFiles)}>
+        {({ getRootProps, getInputProps }) => (
+          <div>
+            <div {...getRootProps()}>
+              <input {...getInputProps()} />
+              <p>Drop Fantasy Pros Superflex csv here</p>
+            </div>
+          </div>
+        )}
+      </Dropzone>
+    );
+  }
 }
